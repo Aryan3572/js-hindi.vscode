52d1707daca47527ca05d36c14688dc5e550c259
Aryan3572 <157047439+Aryan3572@users.noreply.github.com>
2025-10-13 09:52:29 +0000

    conversion and operation

---
+++ .github/basics/03_conversionandOperation.js
@@ -0,0 +1,38 @@
+let score = 33
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueinnumber = Number(score)
+
+console.log(typeof valueinnumber)
+console.log(valueinnumber) 
+
+let Name = "33abc"
+
+console.log(typeof(Name))
+console.log(typeof Name)
+
+
+let valueinName = Number(Name)
+
+console.log(valueinName)
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedin = 1
+
+let booleanisLoggedin = Boolean(isLoggedin)
+
+console.log(booleanisLoggedin)
+console.log(typeof(booleanisLoggedin))
+
+// 1 => true , 0 => false
+// "" => false 
+//   "aryan" => true
+
+let stringnumber = String(score)
+console.log(stringnumber);
+console.log(typeof(stringnumber))
