745fa110d68cc7bdacc564f9593c22ed073258c0
Aryan3572 <157047439+Aryan3572@users.noreply.github.com>
2025-10-13 09:29:53 +0000

    datatypes

--- .github/basics/main1.js
+++ .github/basics/main1.js
@@ -1,1 +1,11 @@
-console.log("aryan raj")
+const accountID = 14425
+let accountName = "My account"
+
+var accountPassword = "12345"
+
+accountCity = "jaipur"
+
+let accountState = "Rajasthan"
+
+
+console.table([accountID,accountName,accountPassword,accountCity,accountState])
