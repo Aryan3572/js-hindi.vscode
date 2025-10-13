1c05c171f5ddc5fd2d3b01dae4db4f3e282928b8
Aryan3572 <157047439+Aryan3572@users.noreply.github.com>
2025-10-13 10:20:09 +0000

    comparison

--- .github/basics/03_conversionandOperation.js
+++ .github/basics/03_conversionandOperation.js
@@ -1,22 +1,22 @@
 let score = 33
 
-console.log(typeof(score))
-console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof score)
 
 let valueinnumber = Number(score)
 
-console.log(typeof valueinnumber)
-console.log(valueinnumber) 
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber) 
 
 let Name = "33abc"
 
-console.log(typeof(Name))
-console.log(typeof Name)
+// console.log(typeof(Name))
+// console.log(typeof Name)
 
 
 let valueinName = Number(Name)
 
-console.log(valueinName)
+// console.log(valueinName)
 
 // 33 => 33
 // "33abc" => NaN
@@ -26,13 +26,46 @@ let isLoggedin = 1
 
 let booleanisLoggedin = Boolean(isLoggedin)
 
-console.log(booleanisLoggedin)
-console.log(typeof(booleanisLoggedin))
+// console.log(booleanisLoggedin)
+// console.log(typeof(booleanisLoggedin))
 
 // 1 => true , 0 => false
 // "" => false 
 //   "aryan" => true
 
 let stringnumber = String(score)
-console.log(stringnumber);
-console.log(typeof(stringnumber))
+// console.log(stringnumber);
+// console.log(typeof(stringnumber))
+
+// ********************************** operations ************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Aryan"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true); // correct
+ 
+// console.log(true+); // wrong
+
+let gamecounter = 100
+gamecounter++
+
+console.log(gamecounter);
