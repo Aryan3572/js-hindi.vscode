1b38d031750f00a203278fe853175c3770680c60
Aryan3572 <157047439+Aryan3572@users.noreply.github.com>
2025-10-13 09:20:04 +0000

    dataypes

---
+++ .github/basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code  as newer version
+
+//alert(3+3) // we ar eusing nodejs , not browser
+
+console.log(3+3)
+
+// datatypes
+
+let name = "aryan"
+let age = 22
+let isLoggedin = false
+
+
+// number => 2 to power 53
+// bigint 
+//string => " "
+//boolean => true/false
+//null => standalone value // for example (let name = " ")
+// undefined => 
+//symbol => unique value
+
+
+//object
+
+console.log(typeof null)
+
+console.log(typeof undefined)
